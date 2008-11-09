19cc3ec6b00ce06d07603205062c6c6ef580d5db
Brian Stolz <brian@tecnobrat.com>
2008-11-09 04:16:25 +0000

    Fixed channel handlers to use new user admin flag

--- modules/channel/core/channel.rb
+++ modules/channel/core/channel.rb
@@ -1,36 +1,30 @@
 class ChannelModule
   def self.join_channel(args, event)
     args = args.split
-    if user = User.find(:first, :include => :hosts, :conditions => ["users.admin = ? and hosts.hostname = ?", 1, event.hostmask])
-      unless channel = Channel.find_by_name(args[0])
-        Channel.create(:name => args[0])
-        @@channels = Channel.find(:all)
-        @@bot.add_channel(args[0])
-        return "Joined #{args[0]}"
-      else
-        @@bot.add_channel(args[0])
-        return "I am already in that channel"
-      end
+    unless Channel.find_by_name(args[0])
+      Channel.create(:name => args[0])
+      @@channels = Channel.find(:all)
+      @@bot.add_channel(args[0])
+      return "Joined #{args[0]}"
+    else
+      @@bot.add_channel(args[0])
+      return "I am already in that channel"
     end
-    return "You can't do that"
   end
   
   def self.part_channel(args, event)
     if event.channel =~ /^\#(.*)$/
-      if user = User.find(:first, :include => :hosts, :conditions => ["users.admin = ? and hosts.hostname = ?", 1, event.hostmask])
-        if channel = Channel.find_by_name(event.channel)
-          channel.destroy
-          @@channels = Channel.find(:all)
-          @@bot.del_channel(event.channel)
-          return "Left #{event.channel}"
-        else
-          @@bot.del_channel(event.channel)
-          return "I am not in that channel"
-        end
+      if channel = Channel.find_by_name(event.channel)
+        channel.destroy
+        @@channels = Channel.find(:all)
+        @@bot.del_channel(event.channel)
+        return "Left #{event.channel}"
+      else
+        @@bot.del_channel(event.channel)
+        return "I am not in that channel"
       end
     else
       return "You need to do that from inside a channel."
     end
-    return "You can't do that"
   end
 end
