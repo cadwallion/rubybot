1314ea077dc72198e994d7401dafdeb945c45188
Brian Stolz <brian@tecnobrat.com>
2008-11-09 04:12:55 +0000

    Needed self!

--- modules/user/models/users.rb
+++ modules/user/models/users.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :hosts
   validates_uniqueness_of :nickname
   def before_save
-    nickname = nickname.downcase
+    self.nickname = self.nickname.downcase
   end
 end
