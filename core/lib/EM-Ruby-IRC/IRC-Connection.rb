8c25dc77b9c96d722ab3f2a3f8c2bc1ed5a6f83f
Brian Stolz <brian@tecnobrat.com>
2009-05-12 16:57:29 +0000

    Needs more logging

--- core/lib/EM-Ruby-IRC/IRC-Connection.rb
+++ core/lib/EM-Ruby-IRC/IRC-Connection.rb
@@ -53,8 +53,9 @@ module IRC
 		def unbind
 			logger.debug("[#{self.name}] Connection lost, sleeping 10 seconds")
 			sleep 10
-			logger.debug("[#{self.name}] Reconnecting")
-			self.reconnect self.setup.config["server_name"], self.setup.config["server_port"].to_i
+			logger.debug("[#{self.name}] Reconnecting to #{setup.config["server_name"]} #{setup.config["server_port"]}")
+			logger.debug("[#{self.name}] #{setup.config.inspect}")
+			self.reconnect setup.config["server_name"], setup.config["server_port"].to_i
 		end
 
 		def add_message_handler(event_type, proc=nil, &handler)
